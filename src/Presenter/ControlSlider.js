90b33a063b12880a47eb8b1c7716b92709c5541a
PinelopiEl <47459702+PinelopiEl@users.noreply.github.com>
2022-12-20 18:33:04 +0200

    Update ControlSlider.js

--- src/Presenter/ControlSlider.js
+++ src/Presenter/ControlSlider.js
@@ -1,42 +1,36 @@
 import React,{useState} from 'react';
 import './Slider.css';
+import {exportMap} from './Map';
+
+var ampm;
+var hour12;
 
 function filter(hour){
-    const filter =  ['==', ['number', ['get', 'Hour']], 12];
-    
-
+    const filter = ['==', ['number', ['get', 'Hour']], 12];
      // update the map
-    // filterBy(hour);
+     exportMap.getMap.setFilter('collisions', ['==', ['number', ['get', 'Hour']], hour]);
+     ampm = hour >= 12 ? 'PM' : 'AM';
+     hour12 = hour % 12 ? hour % 12 : 12;
 
-     // converting 0-23 hour to AMPM format
-     const ampm = hour >= 12 ? 'PM' : 'AM';
-     const hour12 = hour % 12 ? hour % 12 : 12;
-
-     // update text in the UI
-     document.getElementById('active-hour').textContent = hour12 + ampm;
-    
+    // update text in the UI
+    document.getElementById('active-hour').innerText = hour12 + ampm;
 }
-
 function ControlSlider() {
-    const filter =  ['==', ['number', ['get', 'Hour']], 12];
-    // converting 0-23 hour to AMPM format
-    var ampm ;
-    var hour12;
-    /*document.getElementById('slider').addEventListener('input', (event) => {
+
+    document.getElementById('slider').addEventListener('input', (event) => {
         const hour = parseInt(event.target.value);
-        // update the map
-        //map.setFilter('collisions', ['==', ['number', ['get', 'Hour']], hour]);
-        ampm = hour >= 12 ? 'PM' : 'AM';
-        hour12 = hour % 12 ? hour % 12 : 12;
-      
-        // update text in the UI
-        document.getElementById('active-hour').innerText = hour12 + ampm;
-      });*/
+        filter(hour);
+      });
+    
 
     return (
-        <div class="info-box" id='sliderbar' >
-            TRAFFIC DATA :
 
+        <div class="info-box">
+            <h1> Traffic Data</h1>
+            <div class='session' id='sliderbar'>
+                <h2>Hour: <label id='active-hour'>12PM</label></h2>
+                <input id='slider' class='row' type='range' min='0' max='23' step='1' value='12' />
+            </div>
             <div class='session'>
                 <h2>Traffic in route</h2>
                 <div class='row colors'>
@@ -51,42 +45,32 @@ function ControlSlider() {
                 </div>
             </div>
 
-            <div class='session' id='sliderbar'>
-                <h2>Hour: <label id='active-hour'>12PM</label></h2>
-                <input id='slider' class='row' type='range' min='0' max='23' step='1' value='12' />
-            </div>
-            
             <div class='session'>
-            <h2>Day of the week</h2>
-            <div class='row' id='filters'>
-                <input id='all' type='radio' name='toggle' value='all' checked='checked'/>
-                <label for='all'>All</label>
-                <input id='weekday' type='radio' name='toggle' value='weekday'/>
-                <label for='weekday'>Weekday</label>
-                <input id='weekend' type='radio' name='toggle' value='weekend'/>
-                <label for='weekend'>Weekend</label>
-             </div>
-        </div>
-           
+                <h2>Day of the week</h2>
+                <div class='row' id='filters'>
+                    <input id='all' type='radio' name='toggle' value='all' checked='checked'/>
+                    <label for='all'>All</label>
+                    <input id='weekday' type='radio' name='toggle' value='weekday'/>
+                    <label for='weekday'>Weekday</label>
+                    <input id='weekend' type='radio' name='toggle' value='weekend'/>
+                    <label for='weekend'>Weekend</label>
+                </div>
+         </div>
             
         
-        </div> 
+    </div> 
     );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
 export default ControlSlider;
+
+           
+           
+
+            
+            
+            
+
+            
+           
+            
